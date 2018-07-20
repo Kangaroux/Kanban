e3d447feaa40347469b3663a5521b1fda678934a
Jesse Roux <roux.jesse@gmail.com>
2018-07-19 23:39:50 -0400

    Switch to named routes

--- app/static/js/pages/Landing.vue
+++ app/static/js/pages/Landing.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="landing-container">
-    <div class="landing-nav">
-      <router-link to="login">Log in</router-link>
-    </div>
-    <div class="landing-hero">
-      <h1>Landing Page</h1>
-    </div>
+  <div>
+    <router-link :to="{ name: 'login' }">Log in</router-link>
+    <h1>Landing Page</h1>
   </div>
 </template>
--- app/static/js/pages/Login.vue
+++ app/static/js/pages/Login.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    Login page
+    <router-link :to="{ name: 'landing' }">Landing page</router-link>
+    <h1>Login page</h1>
+    <input type="email" v-model="email" placeholder="email" />
+    <input type="password" v-model="password" placeholder="password" />
+    <button @click="login">Log in</button>
   </div>
 </template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        email: "",
+        password: ""
+      };
+    }
+  };
+</script>
